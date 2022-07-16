6d6234984ee02b421ae94f4d355313c6f759d362
cmaspi <chiragmehta1010@gmail.com>
2022-07-16 10:38:49 +0530

    refactored with flake8, mypy

--- Helpful_libs/DataHandler.py
+++ Helpful_libs/DataHandler.py
@@ -1,7 +1,10 @@
-from typing import Tuple
+from typing import Iterable, Tuple
 import numpy as np
 
-def shuffle(train: np.ndarray, labels: np.ndarray):
+
+def shuffle(train: np.ndarray,
+            labels: np.ndarray
+            ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Shuffles the data in a random permutation
     """
@@ -11,21 +14,27 @@ def shuffle(train: np.ndarray, labels: np.ndarray):
     labels = labels[permutation]
     return train, labels
 
-def iterate_minibatches(train : np.ndarray, labels: np.ndarray, batch_size : int):
+
+def iterate_minibatches(train: np.ndarray,
+                        labels: np.ndarray,
+                        batch_size: int
+                        ) -> Iterable[Tuple[np.ndarray, np.ndarray]]:
     """
     Gives a generator object of mini-batches
     """
-    train, labels = shuffle(train,labels)
+    train, labels = shuffle(train, labels)
     for start_idx in range(0, train.shape[0] + 1 - batch_size, batch_size):
         excerpt = slice(start_idx, start_idx+batch_size)
         yield train[excerpt], labels[excerpt]
 
-def split( x : np.ndarray,
-            y : np.ndarray,
-            train : float = 0.7,
-            validation : float = 0.15, 
-            shuffle_bool : bool = True
-            ) -> Tuple[np.ndarray, np.ndarray,np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+def split(x: np.ndarray,
+          y: np.ndarray,
+          train: float = 0.7,
+          validation: float = 0.15,
+          shuffle_bool: bool = True
+          ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray,
+                     np.ndarray, np.ndarray]:
     """
     Returns a train - validation - test split
 
@@ -34,17 +43,18 @@ def split( x : np.ndarray,
         x (np.ndarray): data
         y (np.ndarray): labels
         train (float, optional): fraction of training samples. Defaults to 0.7.
-        validation (float, optional): fraction of validation samples. Defaults to 0.15.
+        validation (float, optional): fraction of validation samples. Defaults
+                                      to 0.15.
         test (float, optional): fraction of test samples. Defaults to 0.15.
 
     Returns:
     --------
-        Tuple[np.ndarray, np.ndarray,np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+        Tuple[np.ndarray, np.ndarray,np.ndarray, np.ndarray, np.ndarray,
+              np.ndarray]
     """
     n = y.size
     a = int(train*n)
     b = int((train+validation)*n)
     if shuffle_bool:
-        x,y = shuffle(x,y)
+        x, y = shuffle(x, y)
     return x[:a], y[:a], x[a:b], y[a:b], x[b:], y[b:]
-    
